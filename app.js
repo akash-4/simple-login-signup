39e4a41afaee624d3bdd43df4f186360ede57cc0
Akash Agarwal <akashagarwal41200@gmail.com>
2020-07-28 00:47:38 +0530

    Login-signup

---
+++ app.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
